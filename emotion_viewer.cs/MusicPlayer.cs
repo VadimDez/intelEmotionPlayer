0ed8cc0bd654a10ab46ea75c0106ecda883a7f7f
Vadym Yatsyuk <vasp3d@gmail.com>
2015-02-24 23:28:55 +0100

    no message

---
+++ emotion_viewer.cs/MusicPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace emotion_viewer.cs
+{
+    class MusicPlayer {
+    
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string lpstrCommand, StringBuilder lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        public void open(string file)
+        {
+            Console.WriteLine("OPPPPEEENNNN!");
+            string command = "open \"" + file + "\" type MPEGVideo alias MyMp3";
+            mciSendString(command, null, 0, 0);
+        }
+
+        public void play()
+        {
+            string command = "play MyMp3";
+            mciSendString(command, null, 0, 0);
+        }
+
+        public void stop()
+        {
+            string command = "stop MyMp3";
+            mciSendString(command, null, 0, 0);
+
+            command = "close MyMp3";
+            mciSendString(command, null, 0, 0);
+        }
+    }
+}
